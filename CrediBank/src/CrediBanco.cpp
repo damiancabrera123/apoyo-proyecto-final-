ee7cfecfbc57bc908df465e184c3fb8ef76a1d42
damiancabrera123 <damian.cabrera@ucsp.edu.pe>
2025-11-21 00:38:06 -0500

    Update CrediBanco.cpp

--- CrediBank/src/CrediBanco.cpp
+++ CrediBank/src/CrediBanco.cpp
@@ -1,12 +1,10 @@
 #include <iostream >
 #include "CrediBanco.h"
 using namespace std;
-CrediBanco::CrediBanco()
-{
-    //ctor
-}
+
 void CrediBanco:: caratula(){
-    cout<<"     //*******************************************************************************************************//"<<endl<<"                        Bienvenido a CrediBanco "<<endl;
+    cout<<"     //*******************************************************************************************************//"<<endl;
+    cout<<"                        Bienvenido a CrediBanco "<<endl;
     cout <<"    Su banco de confianza "<<endl;
     cout<< "    Aqui podra tener sus cuentas dependiendo de sus necesidades "<<endl;
     cout<< "    En nuestro banco usted tendra seguridad y planes de interes a largo plazo"<<endl ;
@@ -25,14 +23,34 @@ void CrediBanco:: caratula(){
     cout<< "    Como siguiente paso vamos a pedirle sus datos para guardarlos en el sistema y asi asignarle su grupo de cuenta "<< endl ;
     cout<< "    Aparte se le dara ciertos beneficios en caso de ser parte de ser usuario premiun o preferencial "<< endl ;
     
-
+Credibanco::Credibanco(std::string numero, double saldoInicial)
+    : numeroCuenta(numero), saldo(saldoInicial) {}
 
 }
+void Credibanco::depositar(double monto) {
+    saldo += monto;
+}
 
+bool Credibanco::retirar(double monto) {
+    if (monto <= saldo) {
+        saldo -= monto;
+        return true;
+    }
+    return false;
+}
+
+double Credibanco::obtenerSaldo() const {
+    return saldo;
+}
+
+void Credibanco::obtenerNumeroCuenta() const {
+    cout<< numeroCuenta;
+}
 CrediBanco::~CrediBanco()
 {
     //dtor
 }
+
 
 
 
