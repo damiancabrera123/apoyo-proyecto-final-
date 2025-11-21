320ae2cc33738a392cbe50c6fe4be3477f6f728d
damiancabrera123 <damian.cabrera@ucsp.edu.pe>
2025-11-21 00:35:26 -0500

    Update CrediBanco.h

--- CrediBank/include/CrediBanco.h
+++ CrediBank/include/CrediBanco.h
@@ -1,20 +1,28 @@
-#include "string_"
 #ifndef CREDIBANCO_H
 #define CREDIBANCO_H
 
-
-class CrediBanco
-{
-    public:
-        CrediBanco();
-        void caratula();
-        virtual ~CrediBanco();
-
-    protected:
+#include <string>
 
 
-    private:
-        string tipo ;
+class Credibanco {
+
+protected:
+    std::string numeroCuenta;
+    double saldo;
+
+public:
+    Credibanco(std::string numeroCuenta, double saldoInicial);
+
+    // Métodos base
+    virtual void depositar(double monto);
+    virtual bool retirar(double monto);
+    double obtenerSaldo() const;
+    void obtenerNumeroCuenta() const;
+
+    // Método virtual para extender en clases hijas
+    virtual void caratula() const;
 };
 
-#endif // CREDIBANCO_H
+#endif
+
+
